adb04fd4112651119b16ec90b0732d4a7752bd67
Mathew Varughese <mat.varughese3@gmail.com>
2019-09-26 21:16:25 -0700

    smart way of generating course data

---
+++ helpers/courses/generate_page_skeletons.js
@@ -0,0 +1,55 @@
+const TEMPLATE =
+`---
+path: "/courses/{{ id }}"
+date: "{{ date }}"
+title: "{{ title }}"
+---
+[Link to SCI]("{{ href }}")
+
+# General Information
+
+# Advice
+
+
+# Prerequisites
+<!-- PREREQ_REPLACEMENT (Do not remove) -->
+
+<!-- END PREREQ_REPLACEMENT (Do not remove) -->
+# Topics Covered
+	- 
+	-
+	-
+# Workload
+
+<!-- TESTIMONIALS
+# Testimonials
+This gets replaced with Gatsby, its
+data comes from Google Sheets for easier
+editing!
+-->
+
+# Resources
+`
+
+const courseList = require("../../src/pages/courses/helpers/course_list.json").courses;
+const fs = require("fs");
+const path = require("path");
+const replaceTemplate = require("../mini_handlebars");
+
+const BASE_PATH = path.join(__dirname, "..", "..", "src", "pages", "courses", "courses_markdown");
+
+function updatePage() {}
+
+courseList.forEach((data) => {
+	const id = data.id;
+	const currentPath = path.join(BASE_PATH, id+".md");
+	if(fs.existsSync(currentPath)) {
+		updatePage(currentPath, data)
+	} else {
+		const toWrite = replaceTemplate(TEMPLATE, {
+			...data,
+			date: new Date().toDateString()
+		})
+		fs.writeFileSync(currentPath, toWrite);
+	}
+})
