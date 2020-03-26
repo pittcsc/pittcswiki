f3033b0a2318a2ba8078198365be7695d1fe753f
Alex D. Zharichenko <azharichenko@gmail.com>
2020-03-26 00:39:42 -0400

    Making courses text a little smaller

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,8 +23,8 @@ const IndexPage = () => (
       justifyContent: 'space-between'
     }}>
       <GroupLink name="Courses" href="courses" />
-      <GroupLink name="Resume" href="resume" />
       <GroupLink name="(In Dev:) Scheduling Classes"/>
+      <GroupLink name="Resume" href="resume" />
       <GroupLink name="(In Dev:) Getting an Interview" />
       <GroupLink name="(In Dev:) Interview Prep" />
       <GroupLink name="(In Dev:) Companies" />
