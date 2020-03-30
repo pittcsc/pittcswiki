dbe504517e627d76423f34541d3f5a9684ba5573
Alex Zharichenko <azharichenko@gmail.com>
2020-03-30 18:16:16 -0400

    Added Made with love

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,11 +1,16 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+
+library.add(fas)
 
 const Footer = () => (
   // gold #CDB87D
   <footer>
-     © {new Date().getFullYear()}, Built by <a href="https://www.pittcsc.org">PittCSC</a>.
+     © {new Date().getFullYear()}, Made with <FontAwesomeIcon icon="heart" color="red"/> by <a href="https://pittcsc.org">PittCSC</a>.
           {` `} (Not affiliated with Pitt CS Department)
           
   </footer>
