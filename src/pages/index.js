a3682bb0d9c635ce6ae62de0bab53f5a770830aa
Alex Zharichenko <azharichenko@gmail.com>
2020-04-05 20:15:20 -0400

    Adding first iteration of index layout and content for CS 1656

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,29 +5,74 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const GroupLink = ({ name, href }) => (
-  <div style={{
-    padding: '20px',
-    display: 'inline-block',
-    marginBottom: '10px'
-  }}>
+  <div >
     <Link to={href}>{ name }</Link>
   </div>
 )
 
+const LinkGroup = ({header}) => (
+  <div>
+    <h3>{header}</h3>
+  </div>
+)
+
+
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>What can we help with?</h1>
+    <h1 style={{ 
+      display: 'flex', justifyContent: 'center'
+      }}>
+        Welcome to the Unoffical Pitt CS Wiki
+    </h1>
+    <hr/>
     <div style={{
       display: 'flex',
       justifyContent: 'space-between'
     }}>
-      <GroupLink name="Courses" href="courses" />
-      <GroupLink name="(In Dev:) Scheduling Classes"/>
-      <GroupLink name="Resume" href="resume" />
-      <GroupLink name="(In Dev:) Getting an Interview" />
-      <GroupLink name="(In Dev:) Interview Prep" />
-      <GroupLink name="(In Dev:) Companies" />
+
+      <table >
+        <tr>
+          <td style={{
+            verticalAlign: 'top',
+            width: '33%'
+          }}>
+            <div>
+              <h3>Classes</h3>
+              <ul>
+                <li><GroupLink name="Courses" href="courses" /></li>
+                <li><GroupLink name="(In Dev:) Scheduling Classes"/></li>
+              </ul>        
+            </div>
+          </td>
+          <td style={{
+            verticalAlign: 'top',
+            width: '33%'
+          }}>
+            <div>
+              <h3>Guides</h3>
+              <ul>
+                <li><GroupLink name="Resume" href="resume" /></li>
+              </ul>
+            </div>
+          </td>
+          <td style={{
+            verticalAlign: 'top',
+            width: '33%'
+          }}>
+            <div>
+              <h3>Career</h3>
+              <ul>
+                <li><GroupLink name="(In Dev:) Getting an Interview" /></li>
+                <li><GroupLink name="(In Dev:) Interview Prep" /></li>
+                <li><GroupLink name="(In Dev:) Companies" /></li>
+              </ul>
+            </div>
+          </td>
+        </tr>
+      </table>
+    
     </div>
   </Layout>
 )
