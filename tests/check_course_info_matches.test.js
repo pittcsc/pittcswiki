ac5513a7ee1b9f6e04251cafd2f896c44aeb115a
Mathew Varughese <mat.varughese3@gmail.com>
2020-05-01 18:57:12 -0400

    create more systematic way to track courses

--- tests/check_course_info_matches.test.js
+++ tests/check_course_info_matches.test.js
@@ -5,15 +5,17 @@ const {
 } = require("../helpers/courses/course_scraper")
 
 test("There are no differences between SCI website and ours", async () => {
-  // If this test fails,
+  // If this test fails there are two possibilites:
+
   // A. Did SCI changed course requirements? If so, see what was added
   // or deleted. Then, run `npm run scrape_courses`. Then,
-  // add the course to pages/courses/markdown
-  //
+  // to generate a new markdown file for this new course, run
+  // `npm run generate_markdown`.
 
   // B. Did SCI update their HTML page? If so, this would break
   // the scraper. So, you need to edit the course_scraper.js file
   // and fix it!
+
   const { remote, local } = await scrapeSciCoursesAndGetLocal()
   expect(remote).toEqual(local)
 })
