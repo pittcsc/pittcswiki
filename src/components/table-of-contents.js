8b9ce9041efe4cc2bef4938eff94893a2dfd8258
Richie Goulazian <rgoulazian@gmail.com>
2020-12-27 18:13:01 -0500

    Add active heading in TOC

--- src/components/table-of-contents.js
+++ src/components/table-of-contents.js
@@ -1,5 +1,6 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import Slugger from "github-slugger"
+import { throttle } from "underscore" 
 
 const slugger = new Slugger()
 
@@ -9,7 +10,39 @@ const truncate = (word) => {
 }
 
 export default function TableOfContents({ headings }) {
+  const [activeHeading, updateActiveHeading] =
+      useState(headings.length > 0 ? headings[0] : null);
+
+  const checkForNewActiveHeading = throttle((e) => {
+    if (headings.length <= 1) {
+      return;
+    }
+
+    let newActiveHeading = headings[0];
+    for (let i = 1; i < headings.length; i++) {
+      const headingElement = document.getElementById(headings[i].id);
+      if (headingElement) {
+        // Plus 10 to avoid roundoff error when clicking TOC links.
+        if (headingElement.offsetTop <= window.scrollY + 10) {
+          newActiveHeading = headings[i];
+        } else {
+          break;
+        }
+      }
+    }
+
+    if (newActiveHeading.id !== activeHeading.id) {
+      updateActiveHeading(newActiveHeading);
+    }
+  }, 200);
+
+  useEffect(() => {
+    window.addEventListener('scroll', checkForNewActiveHeading);
+    return () => window.removeEventListener('scroll', checkForNewActiveHeading);
+  });
+
   if (!headings || headings.length < 4) return null
+
   slugger.reset()
   return (
     <ul className="hidden lg:block fixed lg:left-0 list-none ml-0 markdown-toc">
@@ -18,7 +51,10 @@ export default function TableOfContents({ headings }) {
         .map((heading) => (
           <li
             key={heading.value}
-            className={"toc-heading-depth-" + heading.depth}
+            className={
+              heading.id === activeHeading.id ?
+                  "font-bold toc-heading-depth-" + heading.depth :
+                  "toc-heading-depth-" + heading.depth}
             style={{ marginLeft: heading.depth + "rem" }}
           >
             <a
