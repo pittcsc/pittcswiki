565579f8de9d8c61739c81bd57f028b415cbad90
Richie Goulazian <rgoulazian@gmail.com>
2021-01-06 22:51:24 -0500

    Add external link icon

--- src/components/professor-link.js
+++ src/components/professor-link.js
@@ -1,6 +1,27 @@
 import { useStaticQuery, graphql } from "gatsby";
 import React from "react"
 
+const externalLinkIcon = (
+  <svg
+    aria-hidden="true"
+    focusable="false"
+    data-prefix="fas"
+    data-icon="external-link-alt"
+    role="img"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 512 512"
+    class="inline"
+    style={{
+      width: "0.8em",
+      display: "inline-block",
+      fontSize: "inherit",
+      height: "0.8em",
+      verticalAlign: "0em",
+  }}>
+    <path fill="currentColor" d="M432,320H400a16,16,0,0,0-16,16V448H64V128H208a16,16,0,0,0,16-16V80a16,16,0,0,0-16-16H48A48,48,0,0,0,0,112V464a48,48,0,0,0,48,48H400a48,48,0,0,0,48-48V336A16,16,0,0,0,432,320ZM488,0h-128c-21.37,0-32.05,25.91-17,41l35.73,35.73L135,320.37a24,24,0,0,0,0,34L157.67,377a24,24,0,0,0,34,0L435.28,133.32,471,169c15,15,41,4.5,41-17V24A24,24,0,0,0,488,0Z" class=""></path>
+  </svg>
+)
+
 const ProfessorLink = ({ prof }) => {
   const data = useStaticQuery(graphql`
   query {
@@ -33,7 +54,7 @@ const ProfessorLink = ({ prof }) => {
       target="_blank"
       rel="noopener noreferrer"
       className="tooltip text-black">
-      {profRMPData.professor_full_name}
+      {profRMPData.professor_full_name} {externalLinkIcon}
       <div className="bg-white block border px-4 py-3 shadow-2xl tooltiptext">
         <p className="mb-0">Rate My Professor</p>
         <p className="mb-0">
