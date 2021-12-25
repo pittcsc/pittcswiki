d8129d1e3f7a6171fd961e1fc9d798c27d32de84
Ming <36709118+Zmwang622@users.noreply.github.com>
2021-12-25 01:19:09 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -189,7 +189,7 @@ const LinkPanel = () => {
         />
         <h3>About</h3>
         <p>
-          This wiki was founded by the 2019 Pitt Computer Science Club Officers  
+          This Wiki was created by the 2019 Pitt Computer Science Club Officers  
           and today is still maintained by the club! <Link to="/about">Learn more.</Link>
         </p>
       </div>
