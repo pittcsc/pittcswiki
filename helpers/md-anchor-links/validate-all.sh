dc770db3fbe4af0a1d73d944a9587ff29251d14f
Tobias Hildebrandt <tobiash@tobiash.dev>
2022-02-03 18:38:00 -0500

    md-anchor-links: bash scripts use functions
    improve error handling and output

--- helpers/md-anchor-links/validate-all.sh
+++ helpers/md-anchor-links/validate-all.sh
@@ -2,16 +2,46 @@
 
 # set up failure handling
 set -o errtrace
-trap 'cleanup; echo ERROR: There was an error in ${FUNCNAME-main context}' ERR
+trap 'cleanup; print_error ${FUNCNAME-main context}; exit 1;' ERR
 
 VALIDATE_SCRIPT="helpers/md-anchor-links/validate-md-anchor-links.sh"
 MD_DIR="src/guides"
 
-# find all markdown files
-MD_FILES="$(find \
-    $MD_DIR \
-    -iname "*.md*" \
-    -exec echo {} \;)"
+function init_tmp() {
+    TMP_FILE=$(mktemp) # create temp file to use as output of parallel
+}
 
-# execute in parallel
-parallel "$VALIDATE_SCRIPT {}" ::: "$MD_FILES"
+function cleanup() {
+    rm "$TMP_FILE" # delete temp file
+}
+
+function find_all_md() {
+    # find all markdown files
+    MD_FILES="$(find \
+        $MD_DIR \
+        -iname "*.md*" \
+        -exec echo {} \;)"
+}
+
+function run_tests() {
+    # execute script on files in parallel
+    # any failures (non-zero return values) will be caught by our trap 
+    # that we set up in the beginning, though all files will run
+    parallel "$VALIDATE_SCRIPT {}" ::: "$MD_FILES"
+}
+
+function print_error() {
+    SOURCE=$1 # first argument to this function
+
+    # if error was in run_tests
+    if [[ "$SOURCE" == "run_tests" ]]; then 
+        echo "TEST FAILURE" # assume it was a test that didn't pass
+    else
+        echo "SCRIPT ERROR in $SOURCE" # else assume it was a script error
+    fi
+}
+
+init_tmp
+find_all_md
+run_tests
+cleanup
--- helpers/md-anchor-links/validate-md-anchor-links.sh
+++ helpers/md-anchor-links/validate-md-anchor-links.sh
@@ -4,39 +4,50 @@
 
 # set up failure handling
 set -o errtrace
-trap 'cleanup; echo ERROR: There was an error in ${FUNCNAME-main context}' ERR
+trap 'cleanup; echo SCRIPT ERROR in ${FUNCNAME-main context}; exit 1;' ERR
 
-MD_FILE=$1 # file path is first argument
-TMP_FILE=$(mktemp) # create temp file to use as output of pandoc
+MD_FILE=$1 # file path is first argument to this script
+
+function init_tmp() {
+    TMP_FILE=$(mktemp) # create temp file to use as output of parallel
+}
 
 function cleanup() {
     rm "$TMP_FILE" # delete temp file
 }
 
-# verify file exists
-if [ ! -f "$MD_FILE" ]; then
-    echo "file does not exist: '$MD_FILE'"
-    exit 1
-fi
+function verify_file_exists() {
+    # takes one argument as file path
+    # verify file exists
+    if [ ! -f "$1" ]; then
+        echo "file does not exist: '$1'"
+        exit 1
+    fi
+}
 
-# convert md to pdf using pandoc, catch anchor linking errors
-BROKEN_ANCHOR_LINKS=$( \
-    pandoc "$MD_FILE" --verbose --fail-if-warnings -o "$TMP_FILE".pdf 2>&1 \
-    | grep -E 'pdfTeX warning \(dest\): name{[^}]+}' \
-    | sed -E 's/.*name\{([^}]+)\}.*/\1/' \
-    | sort \
-    | uniq)
+function test_broken_links() {
+    # takes one argument as file path
+    # convert md to pdf using pandoc, catch anchor linking errors
+    BROKEN_ANCHOR_LINKS=$( \
+        pandoc "$1" --verbose --fail-if-warnings -o "$TMP_FILE".pdf 2>&1 \
+        | grep -E 'pdfTeX warning \(dest\): name{[^}]+}' \
+        | sed -E 's/.*name\{([^}]+)\}.*/\1/' \
+        | sort \
+        | uniq)
 
-# if there are some broken links
-if [ -n "$BROKEN_ANCHOR_LINKS" ]; then 
-    echo "broken anchor links in '$MD_FILE':" 
-    for LINK in $BROKEN_ANCHOR_LINKS; do
-        echo "  #$LINK"
-    done
-    exit 1 # failure
-else 
-    echo "all anchor links are valid in '$MD_FILE'"
-fi
+    # if there are some broken links
+    if [ -n "$BROKEN_ANCHOR_LINKS" ]; then 
+        echo "broken anchor links in '$1':" 
+        for LINK in $BROKEN_ANCHOR_LINKS; do
+            echo "  #$LINK"
+        done
+        exit 1 # failure
+    # else 
+    #     echo "all anchor links are valid in '$1'"
+    fi
+}
 
-
+init_tmp
+verify_file_exists "$MD_FILE"
+test_broken_links "$MD_FILE"
 cleanup
